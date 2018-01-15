43900eee10c7c05f5d77f751d24eaaa4c1275b99
Leo Blanc <leo@strataconsulting.com>
2018-01-15 15:52:04 -0300

    Fix

--- .circleci/docker-build.sh
+++ .circleci/docker-build.sh
@@ -6,6 +6,6 @@ DOCKER_IMAGE="leoblanc/wordpress:v${CIRCLE_BUILD_NUM}"
 
 docker build -t "${DOCKER_IMAGE}" .
 
-docker login --username="${DOCKER_USER}" --password="${DOCKER_PASS}" https://hub.docker.com/v1
+docker login --username="${DOCKER_USER}" --password="${DOCKER_PASS}"
 
 docker push "${DOCKER_IMAGE}"
