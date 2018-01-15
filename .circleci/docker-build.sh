4e1809b1a10ee9590bda5bd2c5145ff906e5aa99
Leo Blanc <leo@strataconsulting.com>
2018-01-15 15:38:22 -0300

    Fixed docker login

--- .circleci/docker-build.sh
+++ .circleci/docker-build.sh
@@ -6,6 +6,6 @@ DOCKER_IMAGE=leoblanc/wordpress:v${CIRCLE_BUILD_NUM}
 
 docker build -t ${DOCKER_IMAGE} .
 
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS} hub.docker.com
 
 docker push ${DOCKER_IMAGE}
